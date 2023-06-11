e52d8d359a57bc9629cb987d4192d1116e35e2ce
IbrahimAamer44 <ibrahimaamer920@gmail.com>
2023-06-11 12:35:11 +0500

    Model compile test added

---
+++ Models/TestModel.py
@@ -0,0 +1,9 @@
+
+from LRCN import create_LRCN_model
+
+weights_path = "./Weights/Model_1-Without-CTC-LOSS_checkpoint1_tillbatch_9.h5"
+model = create_LRCN_model()
+
+model.load_weights(weights_path)
+
+print("Model loaded Succesfully compiled !")
